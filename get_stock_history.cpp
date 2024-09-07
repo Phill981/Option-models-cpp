57b5a0af5501644187f5836ed73579e14f89ac78
PhillG <philipp.goetz@fs-students.de>
2024-09-07 16:56:51 +0200

    Added file for implied vola and stock history fetching

---
+++ get_stock_history.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+double calculate_implied_vola(){
+    return 0.0;
+}
+
+std::vector<double> get_historic_data(std::string ticker){
+    std::vector<double> history;
+    return history;
+}
+
+int main(){
+    std::cout << "Test" << std::endl;
+    return 0;
+}
+
